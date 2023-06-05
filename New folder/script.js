d1f8fdaee239bf6599d618a49d459dbdff5fbb1f
Satyen Sriyanshu <satyen4430@gmail.com>
2023-06-05 13:00:24 +0530

    new

--- New folder/script.js
+++
@@ -1,8 +0,0 @@
-const mobileNavToggle = document.getElementById("mobile-toggle");
-const navItems = document.querySelector("nav ul");
-const navMenuBtn = document.querySelector("nav a");
-
-mobileNavToggle.addEventListener("click", function(){
-    navItems.classList.toggle("show");
-    navMenuBtn.classList.toggle("active");
-})
